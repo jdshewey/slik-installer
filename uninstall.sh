212f848c93982c706b4e51573e52ac46743c474d
root <root@slik01.example.com>
2017-10-19 00:16:21 -0600

    Removed angle bracket from uninstall (no redirection needed here)

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 rpm -qa |  sed -e "s/-[-.0-9]\+[.sc1]*\.el7[_0-9]*\..*//g" > /tmp/current_rpmlist
-yum remove $( diff /tmp/current_rpmlist /etc/slik/preinstall_rpmlist | grep "<" | awk '{print $2}' | sed -e "s/-[-.0-9]\+[.sc1]*\.el7[_0-9]*\..*//g"  | egrep -v "\.x86_64$|\.noarch$" | tr '\n' ' ' ) < python-isodate ipxe-bootimgs libstemmer
+yum remove $( diff /tmp/current_rpmlist /etc/slik/preinstall_rpmlist | grep "<" | awk '{print $2}' | sed -e "s/-[-.0-9]\+[.sc1]*\.el7[_0-9]*\..*//g"  | egrep -v "\.x86_64$|\.noarch$" | tr '\n' ' ' ) python-isodate ipxe-bootimgs libstemmer
 rm -f /tmp/current_rpmlist
 if [ $? != 0 ]; then
 	rm -rf /etc/slik
